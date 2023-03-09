fc2f7f9b7f65293d3e9e5e592ba0ade0d466fec9
SPiercer <snowpiercer2001@gmail.com>
2023-03-09 14:39:05 +0200

    made the endpoint public for now

--- src/models/admin/admin.controller.ts
+++ src/models/admin/admin.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Public } from '../../common/decorators/public.decorator';
 import { CreateVendorDto } from '../vendor/dto/create-vendor.dto';
 import { AdminService } from './admin.service';
 
@@ -6,6 +7,7 @@ import { AdminService } from './admin.service';
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
+  @Public()
   @Post('vendor')
   createVendor(@Body() dto: CreateVendorDto): Promise<any> {
     return this.adminService.createVendor(dto)
